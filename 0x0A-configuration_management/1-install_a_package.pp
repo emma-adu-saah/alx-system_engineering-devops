291a3e1ad9d341298b20b33b563d03e5e5d0a61f
emma-adu-saah <aduemma07@gmail.com>
2024-03-23 03:01:30 +0000

    Some changes to task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
 # Installs flask --version 2.1.0 from pip3
 
-package { 'pip3':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path => ['/usr/bin', '/usr/local/bin'],
-  environment => ['PATH=/usr/bin:/usr/local/bin'],
-  unless => '/usr/bin/pip3 show Flask | grep -q  "Version: 2.1.0"',
+package { 'Flask':
+  ensure => '2.1.0',
+  provider => 'pip3',
 }
